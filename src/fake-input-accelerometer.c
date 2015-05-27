6d2fff6c106715b5f9184e2a681ed6bb3023ac5e
Bastien Nocera <hadess@hadess.net>
2015-05-27 11:13:46 +0200

    COPYING: Clarify GPL v3 license
    Closes #26

--- src/fake-input-accelerometer.c
+++ src/fake-input-accelerometer.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2011, 2014 Bastien Nocera <hadess@hadess.net>
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published by
+ * under the terms of the GNU General Public License version 3 as published by
  * the Free Software Foundation.
  */
 
--- src/orientation.h
+++ src/orientation.h
@@ -9,7 +9,7 @@
  *   Lihan Guo <lihan.guo@digia.com>
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published by
+ * under the terms of the GNU General Public License version 3 as published by
  * the Free Software Foundation.
  *
  */
