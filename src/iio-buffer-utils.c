hadess/iio-sensor-proxy

commit 3816a3412043d9026fd38bca4539aeafc2407ad2

buffer-utils: Error out if no sensor is enabled in enable_sensors()
Error out if none of the sensors could be enabled, and throw a warning.

src/iio-buffer-utils.c
@@ -521,7 +521,7 @@ enable_sensors (GUdevDevice *dev,
 	GDir *dir;
 	char *device_dir;
 	const char *name;
-	gboolean ret = TRUE;
+	gboolean ret = FALSE;
 
 	device_dir = g_build_filename (g_udev_device_get_sysfs_path (dev), "scan_elements", NULL);
 	dir = g_dir_open (device_dir, 0, NULL);
@@ -547,14 +547,19 @@ enable_sensors (GUdevDevice *dev,
 		/* Enable */
 		if (write_sysfs_int (name, device_dir, enable) < 0) {
 			g_warning ("Could not enable sensor %s/%s", device_dir, name);
-			ret = FALSE;
 			continue;
 		}
 
+		ret = TRUE;
 		g_debug ("Enabled sensor %s/%s", device_dir, name);
 	}
 	g_dir_close (dir);
 	g_free (device_dir);
+
+	if (!ret) {
+		g_warning ("Failed to enable any sensors for device '%s'",
+			   g_udev_device_get_sysfs_path (dev));
+	}
 
 	return ret;
 }

commit 92fa6ea9ec8b677d00f66c186a209970815e4d2f

buffer-utils: Don't warn when sensor was already enabled
Slight regression in commit 3816a34, where an already enabled sensor
would still make enable_sensors() fail.

src/iio-buffer-utils.c
@@ -539,6 +539,7 @@ enable_sensors (GUdevDevice *dev,
 		/* Already enabled? */
 		path = g_strdup_printf ("scan_elements/%s", name);
 		if (g_udev_device_get_sysfs_attr_as_boolean (dev, path)) {
+			ret = TRUE;
 			g_free (path);
 			continue;
 		}